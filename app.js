c884b378ee57e236996217895365d537140caeac
sydrob <55166840+sydrobhez@users.noreply.github.com>
2024-01-20 14:47:34 -0600

    updated functionality

--- app.js
+++ app.js
@@ -2,7 +2,25 @@ let checkBtn = document.querySelector('#btn');
 let result = document.querySelector('#result');
 
 function checkWord() {
-  alert('is working!');
+  let word = document.querySelector('.input-text').value;
+  let wordLength = word.length;
+  let sentenceStart = word
+    .substring(0, Math.floor(wordLength / 2))
+    .toLowerCase();
+  let sentenceEnd = word
+    .substring(wordLength - Math.floor(wordLength / 2))
+    .toLowerCase();
+
+//   let flip = sentenceEnd.split('').reverse().join('');
+
+// ES6 Syntax
+let flip = [...sentenceEnd].reverse().join('');
+if(sentenceStart === flip) {
+    result.innerHTML = `${word.toUpperCase()} is a palindrome word`;
+} else {
+    result.innerHTML = `${word.toUpperCase()} is NOT a palindrome word`;
+}
+//   alert(flip);
 }
 
 checkBtn.addEventListener('click', checkWord);
